425e1c9bc323bca1c699539396fdecd46e92e0a4
Gerard Gutiérrez <ggutiflo@gmail.com>
2023-01-11 17:16:16 +0100

    implementing freezed

---
+++ lib/app/domain/models/user/user.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    required int id,
+    required String username,
+
+    ///
+    @JsonKey(name: 'avatar', fromJson: avatarPathFromJson) String? avatarPath,
+  }) = _User;
+  const User._();
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
+
+String? avatarPathFromJson(Map<String, dynamic> json) {
+  return json['tmdb']?['avatar_path'] as String?;
+}
